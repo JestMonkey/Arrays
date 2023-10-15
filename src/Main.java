3cd22514bce71d64f1923159fe4c93cad68513a7
M.Wrona <mwrona.66@gmail.com>
2023-10-15 16:58:31 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        TODO zadanie 1
+//        utwórz tablicę ocen z jednego przedmiotu
+//        wydrukuj tablicę
+//        przypisz do kilku jej elementów oceny
+//        wydruje je
+//        oblicz średnią ocen nie używając funkcji wbudowanych
+//        pokaż średnią ocen
+
+//        TODO zadanie 2
+//        utwórz tablicę ocen z 5 przedmiotów
+//        wpisz przykładowe dane
+//        oblicz średnie ocen z każdego przedmiotu
+//        oblicz średnią ocen ze wszystkich przedmiotów
+
+//        TODO zadanie 3
+//        utwórz tablicę przynajmniej 6 nazwisk i miejscowości kolegów z twojej grupy
+//        wydrukuj informację Uczeń ... pochodzi z ..."
+//        spośród wszystkich elementów wydrukuj tylko te, które pochodzą z Tarnobrzega
+
+
+//        TODO 4) biorąc pod uwagę dowolną tablicę int, wypisz ile razy w niej występuje liczba 7?
+
+//        TODO 5) wypisz true, gdy w tablicy pojawi się sekwencja kolejnych liczb 1, 2, 3
+
+//        TODO 6) wypisz true gdy tablica nie zawiera żadnych trójek identycznych liczb po kolei
+
+//        TODO 7) wypisz true, gdy jeden z pierwszych 4 elementów tablicy wynosi 5 (tablica może mieć mniejszy rozmiar)
+
+//        TODO 8) biorąc pod uwagę tablicę policz ile razy wystapi w niej sekwencja cyfry 6 i kolejnej 6 bądź 7
+
+    }
+}
