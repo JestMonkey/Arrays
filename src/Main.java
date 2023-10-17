21470df3d510ac40a57b978a4740ea551b0c7935
M.Wrona <mwrona.66@gmail.com>
2023-10-17 23:13:08 +0200

    korekta

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-//        TODO zadanie 1
+//        TODO 1)
 //        utwórz tablicę ocen z jednego przedmiotu
 //        wydrukuj tablicę
 //        przypisz do kilku jej elementów oceny
@@ -9,13 +9,13 @@ public class Main {
 //        oblicz średnią ocen nie używając funkcji wbudowanych
 //        pokaż średnią ocen
 
-//        TODO zadanie 2
+//        TODO 2)
 //        utwórz tablicę ocen z 5 przedmiotów
 //        wpisz przykładowe dane
 //        oblicz średnie ocen z każdego przedmiotu
 //        oblicz średnią ocen ze wszystkich przedmiotów
 
-//        TODO zadanie 3
+//        TODO 3)
 //        utwórz tablicę przynajmniej 6 nazwisk i miejscowości kolegów z twojej grupy
 //        wydrukuj informację Uczeń ... pochodzi z ..."
 //        spośród wszystkich elementów wydrukuj tylko te, które pochodzą z Tarnobrzega
